4aeccad6be552dc0ea78d3d4ff9a424cc8f7e206
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-05-17 14:58:13 -0400

    testing poke details

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -5,12 +5,25 @@ import PokeInfo from './PokeInfo'
 // Styles
 import '../assets/css/PokeCard.css'
 
+// API
+import {getPokemon} from '../api/index'
+
 export default function PokeCard({pokemon}) {
+
+    const getDetails = async () => {
+        try {
+            const response = await getPokemon(pokemon.url)
+            console.log(response)
+            alert('Full descripcion')
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     return (
         <div 
             className='card'
-            onClick={() => alert('Full descripcion')}
+            onClick={getDetails}
         >
             <PokeImage img={pokemon.name}/>
             <PokeInfo info={pokemon.name}/>
