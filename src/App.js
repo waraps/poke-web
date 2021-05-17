c733dc3fa6d4c86bc714e1bd3678523e76093803
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-05-17 14:47:30 -0400

    testing poke list, poke search and fetch pokemon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
+// API
+import getPokemons from './api/index'
+
+// Components
+import PokeSearch from './components/PokeSearch'
+import PokeList from './components/PokeList'
+
 function App() {
+  const [offset, setOffset] = useState(0)
+  const [pokemons, setPokemons] = useState([])
+  const [listPokemons, setListPokemons] = useState([])
+  const [name, setName] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  
+  useEffect(() => {
+      lisntingPokemon()
+  }, [offset])
+
+  useEffect(() => {
+    findPokemon()
+  }, [name])
+  
+  const lisntingPokemon = async () => {
+      try {
+          setIsLoading(true)
+          const list = await getPokemons(offset)
+          const { results } = list
+          setPokemons([...pokemons, ...results])
+          setListPokemons([...pokemons, ...results])
+      } catch (error) {
+          console.log(error)
+      }
+      setIsLoading(false)
+  }
+
+  const findPokemon = async () => {
+    if(name === '') {
+      setListPokemons(pokemons)
+    } else {
+        let result = pokemons.filter(pokemon => pokemon.name.toLowerCase().indexOf(name.toLowerCase()) > - 1);
+        setListPokemons(result)
+    }
+  }
+
+  const loadMore = () => {
+    setOffset(prev => prev + 20)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PokeSearch pokeName={name} setName={setName}/>
+      <PokeList pokemons={listPokemons}/>
+      <div className='load-more'>
+        {
+          isLoading ? <a className='load-more-btn' onClick={loadMore}>Cargando ..</a>
+                    : <a className='load-more-btn' onClick={loadMore}>Cargar mas</a>
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
