12a9234f590d603e13347dabbfd4e97cc58b8394
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-05-17 14:57:09 -0400

    adding new endpoint (pokemon details)

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,10 @@ const getPokemons = async (offset) => {
     return pokemons
 }
 
-export default getPokemons
+const getPokemon = async (url) => {
+    const response = await fetch(`${url}`)
+    const pokemon = await response.json()
+    return pokemon
+}
+
+export {getPokemons, getPokemon}
