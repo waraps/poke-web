9667038f1c873567f46127334a042c2dd9874f12
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-05-17 15:43:26 -0400

    testing pokemon details

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,32 +1,52 @@
+import React, {useState} from 'react'
+import {
+    Modal,
+    useModal,
+    ModalTransition,
+  } from 'react-simple-hook-modal';
+
 // Components
 import PokeImage from './PokeImage'
 import PokeInfo from './PokeInfo'
+import PokeDetails from './PokeDetails'
 
 // Styles
 import '../assets/css/PokeCard.css'
+import 'react-simple-hook-modal/dist/styles.css';
 
 // API
 import {getPokemon} from '../api/index'
 
 export default function PokeCard({pokemon}) {
+    const [details, setDetails] = useState(null)
+    const { isModalOpen, openModal, closeModal } = useModal();
 
     const getDetails = async () => {
         try {
             const response = await getPokemon(pokemon.url)
             console.log(response)
-            alert('Full descripcion')
+            setDetails(response)
+            openModal()
         } catch (error) {
             console.log(error)
         }
     }
     
     return (
-        <div 
-            className='card'
-            onClick={getDetails}
-        >
-            <PokeImage img={pokemon.name}/>
-            <PokeInfo info={pokemon.name}/>
-        </div>
+            <div 
+                className='card'
+                onClick={getDetails}
+            >
+                <PokeImage img={pokemon.name}/>
+                <PokeInfo info={pokemon.name}/>
+                <Modal
+                  id="any-unique-identifier"
+                  isOpen={isModalOpen}
+                  transition={ModalTransition.SCALE}
+                >
+                  <PokeDetails details={details} />
+                  <button onClick={closeModal}>Cerrar</button>
+                </Modal>
+            </div>
     )
 }
