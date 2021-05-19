94d9cb3c03516d60f62b5abe390f7c1eb7b09ab0
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-05-18 20:07:02 -0400

    adding styles

--- src/components/PokeDetails.js
+++ src/components/PokeDetails.js
@@ -4,44 +4,60 @@ export default function PokeDetails({details}) {
     console.log(details)
     return (
         <div className='details'>
-            <div>
-                <img src={details.sprites.front_default}/>
-                <img src={details.sprites.back_default}/>
-                <h3>{details.name}</h3>
-                <h4>Altura: {details.height}</h4>
-                <h4>Peso: {details.weight}</h4>
-                <h4>Experiencia base: {details.base_experience}</h4>
-
+            <div className='poke-info-container'>
+                <h1 className='poke-name'>{details.name}</h1>
+                <div className='poke-info'>
+                    <div className='poke-image-container'>
+                        <img src={details.sprites.front_default} width='180'/>
+                        <img src={details.sprites.back_default} width='180'/>
+                    </div>
+                    <div className='poke-details-container'>
+                        <div className='poke-details-container-item'>
+                            <h3>Altura:</h3>
+                            <p className='poke-details-container-item-p'>{details.height}</p>
+                        </div>
+                        <div className='poke-details-container-item'>
+                            <h3>Peso:</h3>
+                            <p className='poke-details-container-item-p'>{details.weight}</p>
+                        </div>
+                        <div className='poke-details-container-item'>
+                            <h3>Experiencia base:</h3>
+                            <p className='poke-details-container-item-p'>{details.base_experience}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <strong>Habilidades</strong>
-                {
-                    details.abilities.map(ability => {
-                        return <p key={ability.ability.name}>{ability.ability.name}</p>
-                    })
-                }
+            <div className='poke-abilities-types'>
+                <div className='poke-info-list'>
+                    <h2 className='info-title'>Habilidades</h2>
+                    {
+                        details.abilities.map(ability => {
+                            return <p key={ability.ability.name} className='poke-info-list-item'>{ability.ability.name}</p>
+                        })
+                    }
+                </div>
+                <div className='poke-info-list'>
+                    <h2 className='info-title'>Tipos</h2>
+                    {
+                        details.types.map(type => {
+                            return <p key={type.type.name} className='poke-info-list-item'>{type.type.name}</p>
+                        })
+                    }
+                </div>
             </div>
-            <div>
-                <strong>Tipos</strong>
-                {
-                    details.types.map(type => {
-                        return <p key={type.type.name}>{type.type.name}</p>
-                    })
-                }
-            </div>
-            <div>
-                <strong>Stats</strong>
+            <h2 className='info-title'>Stats</h2>
+            <div className='poke-info-list-stats'>
                 {
                     details.stats.map(stat => {
-                        return <p key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</p>
+                        return <p key={stat.stat.name} className='poke-info-list-item-stats'><strong>{stat.stat.name}:</strong> {stat.base_stat}</p>
                     })
                 }
             </div>
-            <div>
-                <strong>Movimientos</strong>
+            <h2 className='info-title'>Movimientos</h2>
+            <div className='poke-info-list-moves'>
                 {
                     details.moves.map((move, index) => {
-                        return <p key={move.move.name}>#{index} - {move.move.name}</p>
+                        return <p key={move.move.name} className='poke-info-list-moves-item'><strong>#{index}</strong> - {move.move.name} </p>
                     })
                 }
             </div>
